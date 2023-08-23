955ca36da56f1e09954acc3ff708dd70652629b6
Sushil97 <sushil500530@gmail.com>
2023-08-24 00:26:45 +0600

    dot notation and bracket notation explore

---
+++ dot.js
@@ -0,0 +1,11 @@
+const person ={
+    name: 'kodom Ali', 
+    age: 25 ,
+    address: 'Bangladesh',
+    3:'third',
+}
+const prop = 'address' // bracket notation declare
+console.log(person.address) // dot notation 
+console.log(person['address']) // bracket notation 
+console.log(person[prop]) // bracket notation 
+console.log(person['3'])
